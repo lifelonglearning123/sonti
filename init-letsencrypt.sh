6bcb1a4a2a392811d0818fe6d1d10ac46f9fbdcf
Njeru Cyrus <njerucyrus5@hotmail.com>
2026-04-02 22:35:51 +0300

    updated nginx config

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker)" ]; then
   exit 1
 fi
 
-domains=(growthmapper.leonardopower.com)
+domains=(portal.macaws.ai)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="cyrus@macaws.ai" # Adding a valid address is strongly recommended
