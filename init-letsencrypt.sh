f6e143151e49bfc1e5ae7326abf5f053cd006c99
Njeru Cyrus <njerucyrus5@hotmail.com>
2026-04-18 07:38:47 +0300

    updated nginx and gitignore

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker)" ]; then
   exit 1
 fi
 
-domains=(portal.leonardopower.com)
+domains=(crm.theautomate.io)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="cyrus@macaws.ai" # Adding a valid address is strongly recommended
